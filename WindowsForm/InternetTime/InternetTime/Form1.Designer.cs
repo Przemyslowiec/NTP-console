1b099134049c92610c5d7bac04c31f576b642953
Piotr <piotr.kontowicz@interia.pl>
2017-07-29 20:44:18 +0200

    ok

---
+++ WindowsForm/InternetTime/InternetTime/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace InternetTime
+{
+    partial class internet_time
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.time.Location = new System.Drawing.Point(8, 8);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(130, 51);
+            this.time.TabIndex = 1;
+            this.time.Text = "Time:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.tick);
+            // 
+            // internet_time
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 73);
+            this.Controls.Add(this.time);
+            this.Name = "internet_time";
+            this.Text = "InternetTime";
+            this.Load += new System.EventHandler(this.internet_time_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsForm/InternetTime/InternetTime/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetTime
+{
+    public partial class internet_time : Form
+    {
+        public object LblLocalTime { get; private set; }
+
+        public internet_time()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        public static string get_time()
+        {
+                //  const string ntpServer = "pool.ntp.org";
+                var ntp_incoming_data = new byte[48];
+                ntp_incoming_data[0] = 0x1B; //LeapIndicator = 0 (no warning), VersionNum = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+                var addresses = System.Net.Dns.GetHostEntry("194.146.251.101").AddressList;
+                var ip_point = new IPEndPoint(addresses[0], 123);
+                var socket = new System.Net.Sockets.Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                socket.Connect(ip_point);
+                socket.Send(ntp_incoming_data);
+                socket.Receive(ntp_incoming_data);
+                socket.Close();
+
+                ulong int_part = (ulong)ntp_incoming_data[40] << 24 | (ulong)ntp_incoming_data[41] << 16 | (ulong)ntp_incoming_data[42] << 8 | (ulong)ntp_incoming_data[43];
+                ulong fractal_part = (ulong)ntp_incoming_data[44] << 24 | (ulong)ntp_incoming_data[45] << 16 | (ulong)ntp_incoming_data[46] << 8 | (ulong)ntp_incoming_data[47];
+
+                var milliseconds = (int_part * 1000) + ((fractal_part * 1000) / 0x100000000L);
+                var npt_time = (new DateTime(1900, 1, 1)).AddMilliseconds((long)milliseconds);
+
+                TimeSpan offset = TimeZoneInfo.Local.GetUtcOffset(npt_time);
+                npt_time = npt_time.AddHours(offset.Hours);
+           
+            return npt_time.ToString("HH:mm:ss");
+        }
+
+        private void internet_time_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            time.Text = get_time();
+        }
+    }
+}
